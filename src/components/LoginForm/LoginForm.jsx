dfa031654b811ea3052196fa3daf3368fbd838c7
Olga Goncharenko <92627264+GoncharenkoO@users.noreply.github.com>
2022-04-11 09:12:57 +0300

    Update LoginForm.jsx

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,31 +1,19 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { useSelector, shallowEqual, useDispatch } from 'react-redux';
-import { useLocation, useNavigate } from 'react-router-dom';
 import { login } from '../../redux/auth/auth-operations';
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import LoaderComponent from '../Loader';
-import { getIsLoading, getIsLogin } from '../../redux/auth/auth-selectors';
+import { getIsLoading } from '../../redux/auth/auth-selectors';
 import { initialState } from './initialState';
 import styles from './loginForm.module.css';
 
 const LoginForm = () => {
   const [form, setForm] = useState({ ...initialState });
   const isLoading = useSelector(getIsLoading, shallowEqual);
-  const isLogin = useSelector(getIsLogin, shallowEqual);
-  const navigate = useNavigate();
-
-  const location = useLocation();
   const dispatch = useDispatch();
-
-  useEffect(() => {
-    if (isLogin) {
-      const from = location.state?.from || '/contacts';
-      navigate(from);
-    }
-  }, [isLogin]);
 
   const handleChange = ({ target }) => {
     const { name, value } = target;
